5fabc44f8c2920cb3b8eb26af2e33c1ee5ad7eb2
Gerrett84 <Gerrett84@gmail.com>
2013-07-09 18:38:09 +0200

    add phoneapps.mk

---
+++ phoneapps.mk
@@ -0,0 +1,97 @@
+local-phone-apps = $(private-phone-apps) $(private-services)
+private-services := AllshareService \
+					FlashBarService \
+					SuggestionService \
+					SnoteQ1 \
+					Snote_Q1_Provider \
+					ResourceManager \
+					QuickSnote_Q1 \
+					SamsungIME 
+
+private-phone-apps := ApplicationsProvider \
+					  BackupRestoreConfirmation \
+					  BadgeProvider \
+					  BluetoothAvrcp \
+					  BluetoothMap \
+					  BluetoothTest \
+					  CSC \
+					  ClipboardSaveService \
+					  CertInstaller \
+					  DefaultContainerService \
+					  Divx \
+					  EdmVpnServices \
+					  FaceLock \
+					  FactoryTest_Q1_EUR_OPEN \
+					  FmRadio \
+					  KeyChain \
+					  LogsProvider \
+					  MediaUploader \
+					  MusicFX \
+					  MtpApplication \
+					  OmaCP \
+					  PCWClientS \
+					  PRUI \
+					  PanningTryActually \
+					  PhoneErrService \
+					  PopupuiReceiver \
+					  Preconfig \
+					  PreloadInstaller \
+					  SamsungTTS \
+					  SecMediaProvider \
+					  SecHTMLViewer \
+					  SecLiveWallpapersPicker \
+					  SecurityProvider \
+					  SelfTestMode_Q1_EUR_OPEN \
+					  Stk \
+					  UserDictionaryProvider \
+					  VpnDialogs \
+					  VideoPlayer \
+					  WAPPushManager \
+					  WlanTest \
+					  minimode-res \
+					  serviceModeApp_Q1_EUR_OPEN \
+					  wssyncmlnps
+
+private-lockscreen-apps := PickUpTutorial \
+						   AccuweatherDaemon \
+						   AccuweatherWidget
+
+private-phone-apps2 :=  AllshareDMC \
+						AllshareMediaServer \
+						AllSharePlay \
+						BGCleaner \
+						BroadcomMEMSService \
+						CapabilityManagerService \
+						CertInstaller \
+						ChromeBookmarksSyncAdapter \
+						ContextAwareService \
+						CWMManager \
+						DeepSea \
+						DirectShareManager \
+						DrmProvider \
+						DSMForwarding \
+						DSMLawmo \
+						EdmSysScopeService \
+						EdmVpnServices \
+						EnterprisePermissions \
+						Hearingdro \
+						INDI_Server \
+						JobManager \
+						NetworkLocation \
+						NoiseField \
+						OmaCP \
+						OmaDrmPopup \
+						PackageInstaller \
+						PanningTryActually \
+						PCWClientS \
+						PhaseBeam \
+						PRUI \
+						SecurityProvider \
+						ShaoNv \
+						TwDVFSApp \
+						VpnDialogs \
+						WfdBroker \
+						BluetoothAvrcp \
+						BluetoothMap \
+						BluetoothTest
+#DreyeforSamsung
